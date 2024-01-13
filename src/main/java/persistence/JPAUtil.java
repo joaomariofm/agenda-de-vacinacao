a0f0c2ef664e16eacb3c23358e2a766eaca607a0
João Mário <joaomf17p2@gmail.com>
2024-01-13 18:22:29 -0300

    changing db name to "agendadevacinacao" and user to "ufg"

--- src/main/java/persistence/JPAUtil.java
+++ src/main/java/persistence/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-    private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("test");
+    private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("agendadevacinacao");
 
     public static EntityManager getEntityManager() {
         return factory.createEntityManager();
