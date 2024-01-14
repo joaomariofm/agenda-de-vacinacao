47af3d56066b76116e7efa644346847295c84525
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-01-14 20:35:58 -0300

    intenting Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,25 @@ import handlers.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+			HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-						server.createContext("/", new LandingPageHandler());
+			server.createContext("/", new LandingPageHandler());
 
-            server.createContext("/create-user", new CreateUsuarioHandler());
-            server.createContext("/create-allergy", new CreateAlergiaHandler());
-            server.createContext("/create-vaccine", new CreateVacinaHandler());
-            server.createContext("/create-appointment", new CreateAgendaHandler());
+			server.createContext("/create-user", new CreateUsuarioHandler());
+			server.createContext("/create-allergy", new CreateAlergiaHandler());
+			server.createContext("/create-vaccine", new CreateVacinaHandler());
+			server.createContext("/create-appointment", new CreateAgendaHandler());
 
-            server.createContext("/list-users", new ListUsuariosHandler());
-            server.createContext("/list-allergies", new ListAlergiasHandler());
-            server.createContext("/list-vaccines", new ListVacinasHandler());
-            server.createContext("/list-appointments", new ListAgendasHandler());
+			server.createContext("/list-users", new ListUsuariosHandler());
+			server.createContext("/list-allergies", new ListAlergiasHandler());
+			server.createContext("/list-vaccines", new ListVacinasHandler());
+			server.createContext("/list-appointments", new ListAgendasHandler());
 
-            server.start();
+			server.start();
 
-            System.out.println("Server started on port 8080");
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
+			System.out.println("Server started on port 8080");
+		} catch (Exception e) {
+				System.out.println("Error: " + e);
+		}
 	}
 }
