00ce22b9e23f73c89653b9deb8ac51be9b20f255
João Mário <joaomf17p2@gmail.com>
2024-01-13 21:44:36 -0300

    adding build and run scripts

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set root directory
+COMPARISON_DIR_SHA256="64575e4a74f4ef1712de420f9d32d16646ebe48853f0dda902c393fdecd81493"
+ROOT_DIR_SHA256=$(find ../ -name ".root-dir" -exec cat {} +)
+ROOT_DIR_PATH=$(find ../ -name ".root-dir" -exec dirname {} +)
+
+# Set main class
+MAIN_CLASS=$(find $ROOT_DIR_PATH/target/classes -name "Main.class")
+
+if [ "$ROOT_DIR_SHA256" == "$COMPARISON_DIR_SHA256" ]; then
+	if [ -f "$MAIN_CLASS" ]; then
+		java -cp "$ROOT_DIR_PATH/target/classes" Main
+	else
+		echo "Main class not found"
+	fi
+fi
+
